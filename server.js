759d1a1d0eea0d1114e65145587547e9acd2de4b
Will <will_constable@msn.com>
2021-04-13 11:11:29 +0100

    Add basic server set up

---
+++ server.js
@@ -0,0 +1,10 @@
+// Dependencies
+const express = require('express')
+
+// Initiate app
+const app = express()
+
+// Start server
+app.listen(3000, () => {
+  console.log('App running on port 3000')
+})
