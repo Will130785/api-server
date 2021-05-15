9335a2db69d1582824413326ed4ce0cd6456de73
Will <will_constable@msn.com>
2021-05-15 20:59:01 +0100

    Add post route

--- controllers/enquiry/index.js
+++ controllers/enquiry/index.js
@@ -1,6 +1,5 @@
 module.exports.getEnquiry = async (req, res) => {
   // Create data object
-//   const data = req.body
-  console.log('You want to make an enquiry')
-  res.send('You want to make an enquiry')
+  const data = req.body
+  res.send(data)
 }
